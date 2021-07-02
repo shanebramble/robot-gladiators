8cc3969c254d9bdf709f5b748bf794f2d10d72e4
Shane Wade <sbramwade@msn.com>
2021-07-02 19:24:45 -0400

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -79,8 +79,14 @@ var fight = function (enemyName) {
 };
 
 for (var i = 0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    // call fight function with enemy-robot
-    fight(pickedEnemyName);
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        // call fight function with enemy-robot
+        fight(pickedEnemyName);
+    } else {
+        window.alert("You have lost your robot in battle! Game Over!");
+        break;
+    }
 }
